f87692bb957e8a7f480628222c3c8bb1acb3d5eb
aleriiu <aleriiiu@gmail.com>
2026-04-29 21:28:08 +0300

    added hover and small chenges

--- script.js
+++ script.js
@@ -41,51 +41,6 @@ window.addEventListener('scroll', function () {
 
 /***** gallery *****/
 
-// 1. Инициализация плавного скролла Lenis
-// const lenis = new Lenis();
-// function raf(time) {
-//     lenis.raf(time);
-//     requestAnimationFrame(raf);
-// }
-// requestAnimationFrame(raf);
-
-// 2. Настройка GSAP
-// gsap.registerPlugin(ScrollTrigger);
-
-// const tl = gsap.timeline({
-//     scrollTrigger: {
-//         trigger: ".scroll-section",
-//         start: "top top",
-//         end: "bottom bottom",
-//         scrub: 1, // Привязка к скроллу
-//     }
-// });
-
-// Анимация разлета картинок
-// tl.to(".img-2", { x: "-200%", y: "-150%", rotate: -20, opacity: 0 }, 0)
-//   .to(".img-3", { x: "200%", y: "150%", rotate: 20, opacity: 0 }, 0)
-//   .to(".img-4", { x: "-250%", y: "150%", rotate: -30, opacity: 0 }, 0)
-//   .to(".img-5", { x: "250%", y: "-150%", rotate: 30, opacity: 0 }, 0)
-//   .to(".img-6", { x: "250%", y: "-150%", rotate: 30, opacity: 0 }, 0)
-//   .to(".img-7", { x: "250%", y: "-150%", rotate: 30, opacity: 0 }, 0)
-// Масштабируем центральную картинку (эффект пролета сквозь неё)
-//   .to(".img-1", { 
-//       scale: 15, 
-//       opacity: 0, 
-//       duration: 2,
-//       ease: "power2.in" 
-//   }, 0)
-
-// Проявляем текст в конце
-//   .to(".info-content", { 
-//       opacity: 1, 
-//       y: 0, 
-//       duration: 1 
-//   }, "-=0.5");
-
-
-/***** gallery *****/
-
 // 1. Lenis
 const lenis = new Lenis();
 function raf(time) {
@@ -496,7 +451,7 @@ gsap.fromTo(
     ".welcome-text .text-char",
     { color: "#B2B2B2" },
     {
-        color: "#121212",
+        color: "#0E3A61",
         stagger: 0.018,         // задержка между буквами
         ease: "power2.out",
         scrollTrigger: {
@@ -523,7 +478,7 @@ descriptionTl
         ".description-text .text-char",
         { color: "#B2B2B2" },
         {
-            color: "#121212",
+            color: "#0E3A61",
             stagger: 0.018,
             ease: "power2.out",
             duration: 0.6
@@ -533,7 +488,7 @@ descriptionTl
         ".description-text-italic .text-char",
         { color: "#B2B2B2" },
         {
-            color: "#121212",
+            color: "#0E3A61",
             stagger: 0.018,
             ease: "power2.out",
             duration: 0.6
